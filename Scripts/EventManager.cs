0c2c793f902a59d70542580550d6f0600e6f7c7e
Feli <89400571+FeliGame@users.noreply.github.com>
2021-11-04 11:17:11 +0800

    Add many details

--- Scripts/EventManager.cs
+++ Scripts/EventManager.cs
@@ -1,26 +1,26 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using System.IO;
+using System.Net;
 using System.Text;
-using Debug = UnityEngine.Debug;
 
-public static class EventManager
+public static class GiftManager
 {
     private static string json;
-    public const string FileName = "Event List";
-    public static EventPool eventPool;
-    
-    static EventManager()
+    public const string FileName = "Gift List";
+    public static GiftPool giftpool;    //外界调用数据池的入口
+
+    static GiftManager()
     {
         FileToJSON();
         JSONtoObject();
     }
-    
-    public static void LaunchTest() {}  //在TestScript中调用，触发static构造
-    
+
+    public static void LaunchTest() { }  //在TestScript中调用，触发static构造
+
     private static void FileToJSON()  //从Streaming Assets中加载指定json
     {
         string path = Application.streamingAssetsPath + "/" + FileName + ".json";
@@ -34,40 +34,64 @@ public static class EventManager
 
     private static void JSONtoObject()
     {
-        eventPool = new EventPool();
-        eventPool = JsonUtility.FromJson<EventPool>(json);
+        giftpool = new GiftPool();
+        giftpool = JsonUtility.FromJson<GiftPool>(json);
     }
 
-    public static void UpdateAbilities(SingleEvent latestEvent)   //更新人物能力
+    public static void UpdateAbilities(SingleGift selectedGift)   //更新人物能力
     {
-        EventInfluence influence = latestEvent.influence;
-        realizeInfluence(influence.infAge, ref Abilities.Age, Abilities.SpeedAgeUp, Abilities.SpeedAgeDown);
-        realizeInfluence(influence.infEq, ref Abilities.Eq, Abilities.SpeedEqUp, Abilities.SpeedEqDown);
-        realizeInfluence(influence.infHp, ref Abilities.Health, Abilities.SpeedHealthUp, Abilities.SpeedHealthDown);
-        realizeInfluence(influence.infProp, ref Abilities.Property, Abilities.SpeedPropertyUp, Abilities.SpeedPropertyDown);
-        realizeInfluence(influence.infSan, ref Abilities.Sanity, Abilities.SpeedSanityUp, Abilities.SpeedSanityDown);
-        realizeInfluence(influence.infSp, ref Abilities.Spirit, Abilities.SpeedSpiritUp, Abilities.SpeedSpiritDown);
-        realizeInfluence(influence.infIq, ref Abilities.Iq, Abilities.SpeedIqUp, Abilities.SpeedIqDown);
-        realizeInfluence(influence.infKindness, ref Abilities.Kindness, Abilities.SpeedKindnessUp, Abilities.SpeedKindnessDown);
-        //realizeInfluence(influence.infLifespan, ref Abilities.Lifespan, Abilities.SpeedLifespanUp, Abilities.SpeedLifespanDown);
-        realizeInfluence(influence.infLuck, ref Abilities.Luck, Abilities.SpeedLuckUp, Abilities.SpeedLuckDown);
+        GiftInfluence influence = selectedGift.influence;
+        realizeInfluence(influence.infAge, ref Abilities.Age);
+        realizeInfluence(influence.infEq, ref Abilities.Eq);
+        realizeInfluence(influence.infHp, ref Abilities.Health);
+        realizeInfluence(influence.infSp, ref Abilities.Spirit);
+        realizeInfluence(influence.infIq, ref Abilities.Iq);
+        realizeInfluence(influence.infKindness, ref Abilities.Kindness);
+        realizeInfluence(influence.infLifespan, ref Abilities.Lifespan);
+        realizeInfluence(influence.infLuck, ref Abilities.Luck);
+        realizeInfluence(influence.infProp, ref Abilities.Property);
+        realizeInfluence(influence.infSan, ref Abilities.Sanity);
+
+        realizeInfluenceSpeed(influence.speedAgeUp, ref Abilities.SpeedAgeUp);
+        realizeInfluenceSpeed(influence.speedEqUp, ref Abilities.SpeedEqUp);
+        realizeInfluenceSpeed(influence.speedHpUp, ref Abilities.SpeedHealthUp);
+        realizeInfluenceSpeed(influence.speedSpUp, ref Abilities.SpeedSpiritUp);
+        realizeInfluenceSpeed(influence.speedIqUp, ref Abilities.SpeedIqUp);
+        realizeInfluenceSpeed(influence.speedKindnessUp, ref Abilities.SpeedKindnessUp);
+        realizeInfluenceSpeed(influence.speedLuckUp, ref Abilities.SpeedLuckUp);
+        realizeInfluenceSpeed(influence.speedPropUp, ref Abilities.SpeedPropertyUp);
+        realizeInfluenceSpeed(influence.speedSanUp, ref Abilities.SpeedSanityUp);
+
+        realizeInfluenceSpeed(influence.speedAgeDown, ref Abilities.SpeedAgeDown);
+        realizeInfluenceSpeed(influence.speedEqDown, ref Abilities.SpeedEqDown);
+        realizeInfluenceSpeed(influence.speedHpDown, ref Abilities.SpeedHealthDown);
+        realizeInfluenceSpeed(influence.speedSpDown, ref Abilities.SpeedSpiritDown);
+        realizeInfluenceSpeed(influence.speedIqDown, ref Abilities.SpeedIqDown);
+        realizeInfluenceSpeed(influence.speedKindnessDown, ref Abilities.SpeedKindnessDown);
+        realizeInfluenceSpeed(influence.speedLuckDown, ref Abilities.SpeedLuckDown);
+        realizeInfluenceSpeed(influence.speedPropDown, ref Abilities.SpeedPropertyDown);
+        realizeInfluenceSpeed(influence.speedSanDown, ref Abilities.SpeedSanityDown);
     }
 
-    private static void realizeInfluence(string influence, ref float target, float increase, float decrease)
+    private static void realizeInfluence(string influence, ref float target)
     {
-        if (influence == null || influence.Length <= 1) return;
-        
+        if (influence == null || influence.Length <= 1) return;    //空字串表示没有影响
         switch (influence[0])
         {
-            case '+': target += (Int32.Parse(influence.Substring(1)) * increase);
+            case '+':
+                target += Int32.Parse(influence.Substring(1));
                 break;
-            case '-': target -= (Int32.Parse(influence.Substring(1)) * decrease);
+            case '-':
+                target -= Int32.Parse(influence.Substring(1));
                 break;
-            case '*': target *= (Int32.Parse(influence.Substring(1)) * increase);
+            case '*':
+                target *= Int32.Parse(influence.Substring(1));
                 break;
-            case '/': target /= (Int32.Parse(influence.Substring(1)) * decrease);
+            case '/':
+                target /= Int32.Parse(influence.Substring(1));
                 break;
-            case '=': target = Int32.Parse(influence.Substring(1));
+            case '=':
+                target = Int32.Parse(influence.Substring(1));
                 break;
             default:
                 Debug.LogError("Syntax error in influence: " + influence);  //数据池语法错误
@@ -75,114 +99,108 @@ public static class EventManager
         }
     }
 
-}
+    private static void realizeInfluenceSpeed(string influence, ref float target)
+    {
+        if (influence == null || influence.Length <= 1) return;    //空字串表示没有影响
+        switch (influence[0])
+        {
+            case '+':
+                target += float.Parse(influence.Substring(1));
+                break;
+            case '-':
+                target -= float.Parse(influence.Substring(1));
+                break;
+            case '*':
+                target *= float.Parse(influence.Substring(1));
+                break;
+            case '/':
+                target /= float.Parse(influence.Substring(1));
+                break;
+            case '=':
+                target = float.Parse(influence.Substring(1));
+                break;
+            default:
+                Debug.LogError("Syntax error in influenceSpeed: " + influence);  //数据池语法错误
+                break;
+        }
+    }
 
+}
 [Serializable]
-public class EventPool
+public class GiftPool
 {
     public int poolCapacity;
-    public List<SingleEvent> eventList;
+    public List<SingleGift> giftList;
     [NonSerialized] private List<float> _weightList; //概率表（和为1）
-    //[NonSerialized] private List<SingleEvent> _prepareForPick; //该代码旨在移除不满足条件的事件，减少随机数据量
 
-    public List<SingleEvent> FetchEvents(int num) //从池中随机获取指定数量的事件(经典玩法是每次获取一个事件)
+    public List<SingleGift> FetchGifts(int num, bool inheriting = false) //从池中随机获取指定数量的天赋
     {
-        if(Abilities.Age == 0) {    //出生事件
-            List<SingleEvent> birthEvents = new List<SingleEvent>();
-            birthEvents.Add(new SingleEvent());
-            birthEvents[0].content = Abilities.Name + "出生了，是" + Abilities.Gender + "孩子";
-            birthEvents[0].heightRatio = 0.5f;
-            birthEvents[0].widthRatio = 0.5f;
-            birthEvents[0].influence = new EventInfluence();
-            birthEvents[0].influence.infAge = "+1";
-            return birthEvents;
-        }
-        generateWeight(); //O(n)，已过滤了不满足条件的事件
-        Debug.Log("the number of conditional events: " + _weightList.Count);
-        List<SingleEvent> randomEvents = new List<SingleEvent>(); //挑选好的event列表
+        getWeightList(inheriting);   //O(n)
+        bool[] pickList = new bool[_weightList.Count];   //已选为false
+        List<SingleGift> randomGifts = new List<SingleGift>();  //挑选好的gift列表
         for (int i = 0; i < Math.Min(num, _weightList.Count); ++i)
         {
-            int id = RandomWithWeight.GetRandomWithWeight(_weightList);
-            randomEvents.Add(eventList[id]);
+            int id;
+            do
+            {
+                id = RandomWithWeight.GetRandomWithWeight(_weightList);
+            } while (pickList[id]);
+            pickList[id] = true;
+            randomGifts.Add(giftList[id]);
         }
-        
-        return randomEvents;
+        return randomGifts;
     }
-
-    private bool satisfyCondition(int i) //判断是否满足条件
+    private void getWeightList(bool inheriting = false)
     {
-        return eventList[i].condition.ageMax >= Abilities.Age &&
-               eventList[i].condition.ageMin <= Abilities.Age &&
-               eventList[i].condition.eqMax >= Abilities.Eq &&
-               eventList[i].condition.eqMin <= Abilities.Eq &&
-               eventList[i].condition.iqMax >= Abilities.Iq &&
-               eventList[i].condition.iqMin <= Abilities.Iq &&
-               eventList[i].condition.hpMax >= Abilities.Health &&
-               eventList[i].condition.hpMin <= Abilities.Health &&
-               eventList[i].condition.luckMax >= Abilities.Luck &&
-               eventList[i].condition.luckMin <= Abilities.Luck &&
-               eventList[i].condition.propMax >= Abilities.Property &&
-               eventList[i].condition.propMin <= Abilities.Property &&
-               eventList[i].condition.sanMax >= Abilities.Sanity &&
-               eventList[i].condition.sanMin <= Abilities.Sanity &&
-               eventList[i].condition.spMax >= Abilities.Spirit &&
-               eventList[i].condition.spMin <= Abilities.Spirit &&
-               eventList[i].condition.kindnessMax >= Abilities.Kindness &&
-               eventList[i].condition.kindnessMin <= Abilities.Kindness;
-    }
-
-
-    private void generateWeight()
-    {
-        if (eventList == null)
+        if (giftList == null)
         {
-            Debug.Log("Uninitialized eventList!");
+            Debug.LogError("Uninitialized giftList!");
             return;
         }
-
-        int weightSum = 0;
-        for (int i = 0; i < eventList.Count && satisfyCondition(i); ++i)    //将所有满足条件的事件放入
+        if (!inheriting)
         {
-            weightSum += eventList[i].possibility;
+            int weightSum = 0;
+            for (int i = 0; i < giftList.Count; ++i)
+            {
+                weightSum += giftList[i].possibility;
+            }
+
+            _weightList = new List<float>();
+            for (int i = 0; i < giftList.Count; ++i)
+            {
+                _weightList.Add((float)giftList[i].possibility / weightSum);
+            }
         }
-
-        _weightList = new List<float>();
-        for (int i = 0; i < eventList.Count && satisfyCondition(i); ++i)
+        else
         {
-            _weightList.Add( (float)eventList[i].possibility / weightSum);
+            int weightSum = 0;
+            for (int i = 0; i < giftList.Count && !UIGoodGame.inheritedGifts.Contains(giftList[i]) && !Abilities.gifts.Contains(giftList[i]); ++i)
+            {
+                weightSum += giftList[i].possibility;
+            }
+
+            _weightList = new List<float>();
+            for (int i = 0; i < giftList.Count && !UIGoodGame.inheritedGifts.Contains(giftList[i]) && !Abilities.gifts.Contains(giftList[i]); ++i)
+            {
+                _weightList.Add((float)giftList[i].possibility / weightSum);
+            }
         }
     }
 }
+
 [Serializable]
-public class SingleEvent
+public class SingleGift
 {
     public int id;
-    public string content;
+    public string name;
     public int possibility;
-    public string career;
-    public string stage;
-    
-    public float heightRatio;
-    public float widthRatio;
-    
-    public Condition condition;
-    public EventInfluence influence;
+    public GiftInfluence influence;
 }
+
 [Serializable]
-public class Condition
-{
-    public int ageMax, ageMin;
-    public int eqMax, eqMin;
-    public int hpMax, hpMin;
-    public int iqMax, iqMin;
-    public int kindnessMax, kindnessMin;
-    public int luckMax, luckMin;
-    public int propMax, propMin;
-    public int sanMax, sanMin;
-    public int spMax, spMin;
-}
-[Serializable]
-public class EventInfluence
+public class GiftInfluence
 {
     public string infAge, infEq, infHp, infIq, infKindness, infLifespan, infLuck, infProp, infSan, infSp;
+    public string speedAgeUp, speedEqUp, speedHpUp, speedIqUp, speedKindnessUp, speedLuckUp, speedPropUp, speedSanUp, speedSpUp;
+    public string speedAgeDown, speedEqDown, speedHpDown, speedIqDown, speedKindnessDown, speedLuckDown, speedPropDown, speedSanDown, speedSpDown;
 }
